f8b257eeab2f3d615846b446bbaebf9b25a24655
codermother <ozgecoskun_3@hotmail.com>
2021-08-21 14:10:05 +0300

    Add New Components and Display Movies

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import movieReducer from "../features/movie/movieSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
